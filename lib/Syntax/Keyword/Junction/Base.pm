07ce78d5b83dd70458288ca66ffdade6a3ac5d95
Arthur Axel "fREW" Schmidt <frioux+cpan@gmail.com>
2010-11-07 00:22:32 +0000

    Import of FREW/Syntax-Keyword-Junction-0.001000 from CPAN.
    gitpan-cpan-distribution: Syntax-Keyword-Junction
    gitpan-cpan-version:      0.001000
    gitpan-cpan-path:         FREW/Syntax-Keyword-Junction-0.001000.tar.gz
    gitpan-cpan-author:       FREW
    gitpan-cpan-maturity:     released

---
+++ lib/Syntax/Keyword/Junction/Base.pm
@@ -0,0 +1,72 @@
+package Syntax::Keyword::Junction::Base;
+BEGIN {
+  $Syntax::Keyword::Junction::Base::VERSION = '0.001000';
+}
+
+use strict;
+use warnings;
+
+use overload(
+    '=='   => "num_eq",
+    '!='   => "num_ne",
+    '>='   => "num_ge",
+    '>'    => "num_gt",
+    '<='   => "num_le",
+    '<'    => "num_lt",
+    'eq'   => "str_eq",
+    'ne'   => "str_ne",
+    'ge'   => "str_ge",
+    'gt'   => "str_gt",
+    'le'   => "str_le",
+    'lt'   => "str_lt",
+    'bool' => "bool",
+    '""'   => sub {shift},
+);
+
+sub new {
+    my ( $class, @param ) = @_;
+    return bless \@param, $class;
+}
+
+sub values {
+    my $self = shift;
+    return wantarray ? @$self : [ @$self ];
+}
+
+1;
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Syntax::Keyword::Junction::Base
+
+=head1 VERSION
+
+version 0.001000
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Arthur Axel "fREW" Schmidt <frioux+cpan@gmail.com>
+
+=item *
+
+Carl Franks
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Arthur Axel "fREW" Schmidt.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
